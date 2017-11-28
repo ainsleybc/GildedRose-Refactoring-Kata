878ea12585a8f68b083dc560c659534ac9f382c6
ainsleybc <ab.chang@me.com>
2017-11-28 15:31:28 +0000

    last scenario for backstage pass added

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -4,8 +4,8 @@ const Shop = require('../src/gilded_rose').Shop;
 
 describe("Shop", function () {
   
-  var item, updatedItem, shop;
-
+  var item, updatedItem, shop, name;
+  
   beforeEach(function () {
     item = { name: 'foo', quality: 5, sellIn: 5 };
     shop = new Shop([item]);
@@ -13,24 +13,22 @@ describe("Shop", function () {
 
   describe('updateQuality', function () {
     
+
     describe('given that the sell by date has not passed', function () {
 
       it('reduces the sellIn value by 1', function () {
         updatedItem = { name: 'foo', sellIn: 4, quality: 4};        
         expect(shop.updateQuality()).toContain(updatedItem);
       })
-
       it('reduces the quality of an item by 1', function () {
         updatedItem = { name: 'foo', sellIn: 4, quality: 4 };
         expect(shop.updateQuality()).toContain(updatedItem);
       })
-
       it('will not allow the quality to be negative', function () {
         item.quality = 0;
         updatedItem = { name: 'foo', sellIn: 4, quality: 0};
         expect(shop.updateQuality()).toContain(updatedItem);        
       })
-
       it('increases the quality of Brie', function () {
         item.name = 'Aged Brie';
         updatedItem = { name: 'Aged Brie', sellIn: 4, quality: 6 };        
@@ -40,25 +38,42 @@ describe("Shop", function () {
       describe('Backstage passes', function () {
 
         beforeEach(() => {
-          item.name = 'Backstage passes to a TAFKAL80ETC concert';
-        })
-        
+          name = 'Backstage passes to a TAFKAL80ETC concert';
+          item.name = name;
+        })     
+
         it('increases the quality of Backstage passes by 2 if sellIn < 10', function () {
-          var name = item.name;
           item.sellIn = 7;
           updatedItem = { name: name, sellIn: 6, quality: 7 };        
           expect(shop.updateQuality()).toContain(updatedItem);
         })
-
         it('increases the quality of Backstage passes by 3 if sellIn < 5', function () {
-          var name = item.name;
           updatedItem = { name: name, sellIn:4, quality: 8 };        
           expect(shop.updateQuality()).toContain(updatedItem);
         })
 
       })
 
+    })
 
+    describe('given that the sell by date has been reached', function () {
+      
+      beforeEach(() => { item.sellIn = 0; })
+
+      describe('Backstage passes', function () {
+
+        beforeEach(() => {
+          name = 'Backstage passes to a TAFKAL80ETC concert';
+          item.name = name;
+        })     
+        
+        it('reduces the quality to 0 when the sell by date passes', function () {
+          item.sellIn = 0;
+          updatedItem = { name: name, sellIn: -1, quality: 0 };        
+          expect(shop.updateQuality()).toContain(updatedItem);
+        })
+
+      })
     })
 
   })
