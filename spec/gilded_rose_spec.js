e2c8bf880fad90282359a49bca4c2334805180f0
ainsleybc <ab.chang@me.com>
2017-11-28 14:20:26 +0000

    added test for quality reduction - vanilla scenario

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,14 +1,39 @@
 'use strict';
 
-var Shop = require('../src/gilded_rose').Shop;
-var Item = require('../src/item').Item;
+const Shop = require('../src/gilded_rose').Shop;
+const Item = require('../src/item').Item;
 
-describe("Gilded Rose", function() {
+describe("Gilded Rose", function () {
+  
+  var item, shop, items;
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-    const items = gildedRose.updateQuality();
+  beforeEach(function () {
+    item = {name: 'foo'};
+    shop = new Shop([item]);
+  })
+
+  it("should foo", function () {
+    const gildedRose = new Shop([item]);
+    items = gildedRose.updateQuality();
     expect(items[0].name).toEqual("foo");
   });
+
+  describe('updateQuality', function () {
+    
+    describe('sellIn is greater than 0', function () {
+
+      beforeEach(function () {
+        item.sellIn = 5;
+      })
+
+      it('should reduce the quality of an item by 1', function () {
+        item.quality = 5;
+        shop.updateQuality()
+        expect(item.quality).toEqual(4);
+      })
+
+    })
+
+  })
 
 });
