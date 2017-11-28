5313fb7a81eee08432eb71fdfc3ce3398d528a6c
ainsleybc <ab.chang@me.com>
2017-11-28 15:02:46 +0000

    added test for Aged Brie

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -19,16 +19,28 @@ describe("Shop", function () {
         item.sellIn = 5;
       })
 
+      it('reduces the sellIn value by 1', function () {
+        var updatedItem = { name: 'foo', sellIn: 4};        
+        expect(shop.updateQuality()).toContain(updatedItem);
+      })
+
       it('reduces the quality of an item by 1', function () {
         item.quality = 5;
         var updatedItem = { name: 'foo', sellIn: 4, quality: 4 };
         expect(shop.updateQuality()).toContain(updatedItem);
       })
 
-      it('will not allow the quality to be less than 0', function () {
+      it('will not allow the quality to be negative', function () {
         item.quality = 0;
         var updatedItem = { name: 'foo', sellIn: 4, quality: 0};
         expect(shop.updateQuality()).toContain(updatedItem);        
+      })
+
+      it('increases the quality of Brie', function () {
+        item.name = 'Aged Brie';
+        item.quality = 5;
+        var updatedItem = { name: 'Aged Brie', sellIn: 4, quality: 6 };        
+        expect(shop.updateQuality()).toContain(updatedItem);
       })
 
     })
