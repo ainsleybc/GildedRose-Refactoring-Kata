abffe27851880c19eee699491d73073b5094a4a4
ainsleybc <ab.chang@me.com>
2017-11-28 14:45:53 +0000

    added test for quality < 0

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -19,10 +19,16 @@ describe("Shop", function () {
         item.sellIn = 5;
       })
 
-      it('should reduce the quality of an item by 1', function () {
+      it('reduces the quality of an item by 1', function () {
         item.quality = 5;
         var updatedItem = { name: 'foo', sellIn: 4, quality: 4 };
         expect(shop.updateQuality()).toContain(updatedItem);
+      })
+
+      it('will not allow the quality to be less than 0', function () {
+        item.quality = 0;
+        var updatedItem = { name: 'foo', sellIn: 4, quality: 0};
+        expect(shop.updateQuality()).toContain(updatedItem);        
       })
 
     })
